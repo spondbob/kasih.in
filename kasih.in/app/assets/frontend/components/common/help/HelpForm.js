f5ad64229d52ab1b0c93c6d4660b51792103b15c
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-06-06 00:23:35 +0800

    Separated presentational from containers.

--- kasih.in/app/assets/frontend/components/common/help/HelpForm.js
+++ kasih.in/app/assets/frontend/components/common/help/HelpForm.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as helpActions from '../../../actions/helpActions';
+import HelpFormUI from './presentational/HelpForm';
 
 class HelpForm extends React.Component{
   constructor(props, context) {
@@ -11,41 +12,11 @@ class HelpForm extends React.Component{
       help: { message: "" }
     };
 
-    this.onMessageChange = this.onMessageChange.bind(this);
-    this.onClickPost = this.onClickPost.bind(this);
-  }
-
-  onMessageChange(event){
-    const help = this.state.help;
-    help.message = event.target.value;
-    this.setState({help: help});
-  }
-
-  onClickPost(event){
-      this.props.actions.createHelp(this.state.help);
-  }
-
-  helpRow(help, index){
-    return <div key={index}>{help.message}</div>;
   }
 
   render(){
     return (
-      <div>
-        <h2>Need help?</h2>
-        <input
-          type="text"
-          onChange={this.onMessageChange}
-          value={this.state.help.message} />
-        <input
-          type="submit"
-          value="Post"
-          className="btn btn-primary"
-          onClick={this.onClickPost} />
-        <h3>Helps</h3>
-        {this.props.helps.map(this.helpRow)}
-      </div>
-
+      <HelpFormUI actions={this.props.actions} helps={this.props.helps} />
     );
   }
 }
