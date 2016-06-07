b35642b662d9bda3710c9052c4281ac7f0ec9a09
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-06-07 23:46:20 +0800

    Change home page view.

--- kasih.in/app/assets/frontend/components/home/HomePage.js
+++ kasih.in/app/assets/frontend/components/home/HomePage.js
@@ -4,12 +4,16 @@ import {Link} from 'react-router';
 class HomePage extends React.Component{
   render(){
     return (
-      <div className="jumbotron">
-        <h1>Kasih.in</h1>
-        <p>Helping people is easy.</p>
-        <div className="wrapper">
-            <Link to="about" align="center" className="btn btn-primary btn-large">Learn more</Link>
-            <Link to="help" align="center"  className="btn btn-primary btn-large">Try now</Link>
+      <div className="row">
+        <div className="col s12">
+          <h1 className="center-align">Kasih.in</h1>
+          <h4 className="center-align">Helping people is easy.</h4>
+          <div className="col s6 right-align">
+            <Link className="center-align" to="about" align="center" className="btn btn-primary btn-large">Learn more</Link>
+          </div>
+          <div className="col s6 left-align">
+            <Link className="center-align" to="help" align="center"  className="btn btn-primary btn-large">Try now</Link>
+          </div>
         </div>
       </div>
     );
