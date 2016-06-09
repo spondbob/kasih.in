90d97ee6d0c70b6b45f8337add9a16c01e3bd792
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-06-10 01:29:41 +0800

    Add placeholder modal to show details.

--- kasih.in/app/assets/frontend/components/common/help/presentation/HelpFeedItem.js
+++ kasih.in/app/assets/frontend/components/common/help/presentation/HelpFeedItem.js
@@ -1,20 +1,22 @@
 import React, {PropTypes} from 'react';
+import HelpFeedModal from './HelpFeedModal';
 
 export default class HelpFeedItem extends React.Component {
+
   render() {
     const {
       item
     } = this.props;
 
     return (
-      <li className="collection-item avatar">
-        <img src="images/yuna.jpg" alt="" className="circle" />
-        <span className="title">{item.name}</span>
-        <p>{item.message}<br />
-        </p>
-        <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-      </li>
 
+        <li className="collection-item avatar">
+            <img src="images/yuna.jpg" alt="" className="circle" />
+            <span className="title">{item.name}</span>
+            <p>{item.message}<br />
+            </p>
+            <i className="material-icons secondary-content">grade</i>
+        </li>
 
     );
   }
--- kasih.in/app/assets/frontend/components/help/HelpPage.js
+++ kasih.in/app/assets/frontend/components/help/HelpPage.js
@@ -5,12 +5,18 @@ import {getHelpsFeed} from '../../actions/helpActions';
 import HelpForm from '../common/help/HelpForm';
 import HelpFeedItem from '../common/help/presentation/HelpFeedItem';
 
+// FIXME:  Don't use react-materialize as it is unstable and not really
+// modifiedable.Better to use plain JSX
+
+import {Modal} from 'react-materialize';
+
 function loadData(props) {
   props.getHelpsFeed();
 }
 
 class HelpPage extends React.Component {
-  componentWillMount() {
+
+  componentDidMount() {
     loadData(this.props);
   }
 
@@ -19,11 +25,23 @@ class HelpPage extends React.Component {
     let i = 0;
     return (
       <div>
-        <HelpForm />
+        <div>
+          <HelpForm />
+        </div>
         <div className="container">
-          <ul className="collection collapsible" data-collapsible="accordion">
+          <ul className="collection">
             {items.map(item =>
-              <HelpFeedItem key={item.id} item={item} />
+              <Modal key={item.id}
+                header={item.name}
+                fixedFooter
+                trigger={
+                  <a>
+                    <HelpFeedItem key={item.id} item={item} />
+                  </a>
+                }>
+                // TODO: Placeholder text. Change this to use props instead.
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+              </Modal>
             )}
           </ul>
         </div>
