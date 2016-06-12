e75a5733d4657466886d6cc84bfb6454a7203dc7
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-06-12 19:55:38 +0800

    Add styling and some navigation to header.

--- kasih.in/app/assets/frontend/components/common/Header.js
+++ kasih.in/app/assets/frontend/components/common/Header.js
@@ -7,13 +7,27 @@ import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import { darkWhite } from 'material-ui/styles/colors';
+import typography from 'material-ui/styles/typography';
 
-const Header = () => {
-  return (
-    <AppBar
+const styles = {
+  appBar: {
+    position: 'fixed',
+    top: 0,
+  },
+};
+
+const Header = () => (
+  <AppBar
     title="Kasih.in"
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+    style={styles.root}
+    iconElementLeft={
+    <IconButton
+      containerElement={<Link to="/" />}>
+      <NavigationClose />
+    </IconButton>}
     iconElementRight={
+      //TODO: Change the icon once you have
       <IconMenu
         iconButtonElement={
           <IconButton><MoreVertIcon /></IconButton>
@@ -21,13 +35,14 @@ const Header = () => {
         targetOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
       >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
+        <MenuItem 
+          primaryText="About"
+          containerElement={<Link to="/about" />}
+        />
         <MenuItem primaryText="Sign out" />
       </IconMenu>
     }
-    />
-  );
-};
+    /> 
+);
 
 export default Header;
